a5150bfdad0701f4a148eae4583b25a54377f24a
Aleksei Marov <alekseymmm@mail.ru>
2020-04-18 19:33:58 +0300

    add allocation for dp

--- pathOnGrid/pathOnGrid.go
+++ pathOnGrid/pathOnGrid.go
@@ -1,7 +1,40 @@
 package pathongrid
 
-func shortestPath(grid [][]int, k int) int {
+import "math"
 
+func allocateDPSolution(grid [][]int, k int) [][][]int {
+	dpSolution := make([][][]int, len(grid))
+	for i := 0; i < len(grid); i++ {
+		dpSolution[i] = make([][]int, len(grid[0]))
+		for j := 0; j < len(grid[0]); j++ {
+			ar := make([]int, k)
+			for l := range ar {
+				ar[l] = math.MaxInt32
+			}
+			dpSolution[i][j] = ar
+		}
+	}
+	return dpSolution
+}
+
+func allocateVisited(grid [][]int) [][]bool {
+	visited := make([][]bool, len(grid))
+	for i := 0; i < len(grid); i++ {
+		visited[i] = make([]bool, len(grid[0]))
+
+		for j := range visited[i] {
+			visited[i][j] = false
+		}
+	}
+	return visited
+}
+
+func shortestPath(grid [][]int, k int) int {
+	dpSolution := allocateDPSolution(grid, k)
+	visited := allocateVisited(grid)
+
+	println(dpSolution, visited)
+	return 0
 }
 
 func ShortestPath(grid [][]int, k int) int {
